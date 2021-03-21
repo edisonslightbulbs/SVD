c3687f78ae6195b35a5115fc5358fc7b42631ede
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-03-21 17:25:03 +0100

    improving code readability

--- src/svd.cpp
+++ src/svd.cpp
@@ -32,27 +32,23 @@ std::vector<Point> svd::compute(std::vector<Point>& points)
     Eigen::Vector3d n(normalMat(0, 0), normalMat(1, 0), normalMat(2, 0));
 
     /** container for growing region of interest */
-    std::vector<Point> roi;
+    std::vector<Point> proposal;
 
     /** if the point-norm form is satisfied, corresponding point sits on plane */
     const double E_MAX = 100.0;
     const double E_MIN = -130.0;
 
     int index = 0;
-    int id = 0;
     for(auto point: points){
         Eigen::Vector3d v(pointsMat(index, xCol), pointsMat(index, yCol), pointsMat(index, zCol));
 
         /** grow region */
         if (n.dot(v) < E_MAX && n.dot(v) > E_MIN) {
-            /** assign new ids' */
-            point.m_id = id;
-                roi.push_back(point);
-            id ++;
+                proposal.push_back(point);
             }
             index ++;
         }
         LOG(INFO) << timer.getDuration() << " ms: region growing";
 
-        return roi;
+        return proposal;
     }
