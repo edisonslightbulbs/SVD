02bdd5f40fafe8a69c406dd51b783e2a8c2f0b7a
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-04-09 15:52:40 +0200

    improving implementation learnability

--- src/svd.cpp
+++ src/svd.cpp
@@ -1,30 +1,29 @@
 #include "svd.h"
 
-std::pair<Eigen::JacobiSVD<Eigen::MatrixXd>, Eigen::MatrixXd> svd::compute(
+std::pair<Eigen::JacobiSVD<Eigen::MatrixXf>, Eigen::MatrixXf> svd::compute(
     std::vector<Point>& points)
 {
     /** compute centroid */
     Point centroid = Point::centroid(points);
 
     /** represent points as matrix */
-    Eigen::MatrixXd translatedPointsMat(points.size(), R);
+    Eigen::MatrixXf vectors(points.size(), R);
     int row = 0;
     for (auto point : points) {
-        translatedPointsMat(row, xCol) = point.m_x;
-        translatedPointsMat(row, yCol) = point.m_y;
-        translatedPointsMat(row, zCol) = point.m_z;
+        vectors(row, xCol) = point.m_x;
+        vectors(row, yCol) = point.m_y;
+        vectors(row, zCol) = point.m_z;
         row++;
     }
     /**   translate points to direction vectors */
-    translatedPointsMat.col(xCol).array() -= centroid.m_x;
-    translatedPointsMat.col(yCol).array() -= centroid.m_y;
-    translatedPointsMat.col(zCol).array() -= centroid.m_z;
+    vectors.col(xCol).array() -= centroid.m_x;
+    vectors.col(yCol).array() -= centroid.m_y;
+    vectors.col(zCol).array() -= centroid.m_z;
 
     /** compute svd */
     int setting = Eigen::ComputeThinU | Eigen::ComputeThinV;
-    Eigen::JacobiSVD<Eigen::MatrixXd> svd
-        = translatedPointsMat.jacobiSvd(setting);
+    Eigen::JacobiSVD<Eigen::MatrixXf> svd = vectors.jacobiSvd(setting);
 
     /** return computed svd and translated points matrix */
-    return { svd, translatedPointsMat };
+    return { svd, vectors };
 }
