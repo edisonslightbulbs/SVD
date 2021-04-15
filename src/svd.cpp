c63c522311a9c24955c9d1823b49bdf3533d4dcc
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-04-15 14:29:41 +0200

    updating point typedef

--- src/svd.cpp
+++ src/svd.cpp
@@ -10,15 +10,15 @@ std::pair<Eigen::JacobiSVD<Eigen::MatrixXf>, Eigen::MatrixXf> svd::compute(
     Eigen::MatrixXf vectors(points.size(), R);
     int row = 0;
     for (auto point : points) {
-        vectors(row, xCol) = point.m_x;
-        vectors(row, yCol) = point.m_y;
-        vectors(row, zCol) = point.m_z;
+        vectors(row, xCol) = point.m_xyz[0];
+        vectors(row, yCol) = point.m_xyz[1];
+        vectors(row, zCol) = point.m_xyz[2];
         row++;
     }
     /**   translate points to direction vectors */
-    vectors.col(xCol).array() -= centroid.m_x;
-    vectors.col(yCol).array() -= centroid.m_y;
-    vectors.col(zCol).array() -= centroid.m_z;
+    vectors.col(xCol).array() -= centroid.m_xyz[0];
+    vectors.col(yCol).array() -= centroid.m_xyz[1];
+    vectors.col(zCol).array() -= centroid.m_xyz[2];
 
     /** compute svd */
     int setting = Eigen::ComputeThinU | Eigen::ComputeThinV;
