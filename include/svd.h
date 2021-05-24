58682cfe1134e64c0b827fe8f3f7b497a5f9290d
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-05-24 13:23:13 +0200

    class implementation

--- include/svd.h
+++ include/svd.h
@@ -7,8 +7,22 @@
 
 #include "point.h"
 
-namespace svd {
-std::pair<Eigen::JacobiSVD<Eigen::MatrixXf>, Eigen::MatrixXf> compute(
-    std::vector<Point>& points);
+class svd {
+
+private:
+    std::vector<Point> m_points;
+    const int C0 = 0; // single column
+    const int C1 = 1; // single column
+    const int C2 = 2; // single column
+public:
+    Point m_centroid;
+    Eigen::MatrixXf m_vectors;
+    Eigen::JacobiSVD<Eigen::MatrixXf> m_usv;
+
+    explicit svd(const std::vector<Point>& points);
+
+    Eigen::Vector3d getV3Normal();
+
+    std::vector<Eigen::Vector3d> getUNormals();
 };
 #endif /* SVD_H */
--- src/svd.cpp
+++ src/svd.cpp
@@ -1,29 +1,55 @@
 #include "svd.h"
 
-std::pair<Eigen::JacobiSVD<Eigen::MatrixXf>, Eigen::MatrixXf> svd::compute(
-    std::vector<Point>& points)
-{
-    /** compute centroid */
-    Point centroid = Point::centroid(points);
+svd::svd(const std::vector<Point>& points): m_points(points) {
+    // compute centroid
+    m_centroid = Point::centroid(m_points);
 
-    /** represent points as matrix */
-    Eigen::MatrixXf vectors(points.size(), R);
+    // cast points to eigen matrix
+    const int R = points.size();
+    const int C = 3;
+    m_vectors = Eigen::MatrixXf(R, C);
     int row = 0;
     for (auto point : points) {
-        vectors(row, xCol) = (float)point.m_xyz[0];
-        vectors(row, yCol) = (float)point.m_xyz[1];
-        vectors(row, zCol) = (float)point.m_xyz[2];
+        m_vectors(row, C0) = (float)point.m_xyz[0];
+        m_vectors(row, C1) = (float)point.m_xyz[1];
+        m_vectors(row, C2) = (float)point.m_xyz[2];
         row++;
     }
-    /**   translate points to direction vectors */
-    vectors.col(xCol).array() -= (float)centroid.m_xyz[0];
-    vectors.col(yCol).array() -= (float)centroid.m_xyz[1];
-    vectors.col(zCol).array() -= (float)centroid.m_xyz[2];
 
-    /** compute svd */
-    int setting = Eigen::ComputeThinU | Eigen::ComputeThinV;
-    Eigen::JacobiSVD<Eigen::MatrixXf> svd = vectors.jacobiSvd(setting);
+    // translate points to directional vectors using centroid
+    m_vectors.col(C0).array() -= (float)m_centroid.m_xyz[0];
+    m_vectors.col(C1).array() -= (float)m_centroid.m_xyz[1];
+    m_vectors.col(C2).array() -= (float)m_centroid.m_xyz[2];
 
-    /** return computed svd and translated points matrix */
-    return { svd, vectors };
+    // set flags for svd computation
+    int option = Eigen::ComputeThinU | Eigen::ComputeThinV;
+
+    // compute singular value decomposition
+    m_usv = m_vectors.jacobiSvd(option);
+}
+
+Eigen::Vector3d svd::getV3Normal(){
+    Eigen::MatrixXf normal = m_usv.matrixV().col(2);
+    Eigen::Vector3d m_v3Norm = Eigen::Vector3d(normal(0, C0), normal(1, C0), normal(2, C0));
+    return m_v3Norm;
+}
+
+std::vector<Eigen::Vector3d> svd::getUNormals(){
+    // get normals computed for each of the X, Y, and Z vectors
+    Eigen::MatrixXf X = m_usv.matrixU().col(0);
+    Eigen::MatrixXf Y = m_usv.matrixU().col(1);
+    Eigen::MatrixXf Z = m_usv.matrixU().col(2);
+
+    // assert # of computed U normals tallies with # of points
+    assert(m_points.size() == m_usv.matrixU().rows());
+
+    // collect normal vectors for each point
+    int i = 0;
+    std::vector<Eigen::Vector3d> normals;
+    for(const auto& point : m_points) {
+        Eigen::Vector3d normal( X(i, C0), Y(i, C1), Z(i, C2) );
+        normals.emplace_back(normal);
+        i++;
+    }
+    return normals;
 }
