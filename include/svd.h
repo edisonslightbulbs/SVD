f88620fae4d3c926f032624c8b054da94588c26a
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-05-24 13:41:43 +0200

    adding doc

--- include/svd.h
+++ include/svd.h
@@ -10,19 +10,40 @@
 class svd {
 
 private:
+    const int C0 = 0; // column 0
+    const int C1 = 1; // column 1
+    const int C2 = 2; // column 2
     std::vector<Point> m_points;
-    const int C0 = 0; // single column
-    const int C1 = 1; // single column
-    const int C2 = 2; // single column
+
 public:
-    Point m_centroid;
-    Eigen::MatrixXf m_vectors;
-    Eigen::JacobiSVD<Eigen::MatrixXf> m_usv;
+    Point m_centroid;                        // centroid of given set of points
+    Eigen::MatrixXf m_vectors;               // points translated from centroid
+    Eigen::JacobiSVD<Eigen::MatrixXf> m_usv; // computed USV solution
 
-    explicit svd(const std::vector<Point>& points);
+    /** svd
+     *    Constructs and a singular value decomposition
+     *    USV solution, i.e., the
+     *    1. [ U ] the unitary matrix,
+     *    2. [ S ] rectangular diagonal matrix, and
+     *    3. [ V ] the complex unitary matrix.
+     *
+     * @param points
+     *   Give set of points.
+     */
+    explicit svd(const std::vector<Point>& points, const int& flag);
 
+    /** getV3Normal
+     *
+     * @retval
+     *   Normal vector from the complex unitary matrix.
+     */
     Eigen::Vector3d getV3Normal();
 
+    /** getUNormal
+     *
+     * @retval
+     *   Normal vectors from the unitary matrix.
+     */
     std::vector<Eigen::Vector3d> getUNormals();
 };
 #endif /* SVD_H */
